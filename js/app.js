ee3d910642e6269febe9380e0d9b882cecd96ffe
antoniamescia <antonia.mescia.novarese@gmail.com>
2021-11-21 18:10:42 -0300

    Entrega 1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let nombre = prompt('Ingresa tu nombre:')
+let apellido = prompt('Ingresa tu apellido:')
+let musica = prompt('Ingresa tu género musical favorito:').toLowerCase()
+let comida = prompt('Ingresa tu comida favorita:').toLowerCase()
+let color = prompt('Ingresa tu color favorito:').toLowerCase()
+
+alert('¡Hola, soy ' + nombre + ' ' + apellido + '! Me gusta escuchar ' + musica + ', me encanta comer ' + comida + ' y mi color favorito es ' + color + '.') 
+
